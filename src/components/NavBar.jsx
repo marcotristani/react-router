aff7f9105fa7f934503de6245d3692fe7294b82b
marcotristani <marco.tristani@hotmail.it>
2026-01-28 14:25:39 +0100

    flat: creato css per active navbar e generata navlink con map

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,21 @@
 import { Link, NavLink } from "react-router-dom";
 
 function NavBar() {
+  const links = [
+    { path: "/AboutUs", label: "Chi siamo" },
+    { path: "/ProductList", label: "Lista Prodotti" },
+  ];
   return (
-    <ul>
-      <li>
-        <Link to={"/"}>Homepage</Link>
-      </li>
-      <li>
-        <NavLink to={"/AboutUs"}>Chi siamo</NavLink>
-      </li>
-      <li>
-        <NavLink to={"/ProductList"}>Lista Prodotti</NavLink>
-      </li>
-    </ul>
+    <nav>
+      <Link to={"/"}>Homepage</Link>
+      <ul>
+        {links.map((link) => (
+          <li>
+            <NavLink to={link.path}>{link.label}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
 
