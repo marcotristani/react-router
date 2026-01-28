2ab4f718a399355840124ecfac67d3c040a7e301
marcotristani <marco.tristani@hotmail.it>
2026-01-28 15:41:30 +0100

    flat:effettuata chiamata axios e stampata lista prodotti in pagina ListProducts

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,9 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import NavBar from "../components/NavBar";
+import { use } from "react";
+
+const endpoint = "https://fakestoreapi.com/products";
+
 function ProductList() {
+  const [productsList, setProductsList] = useState([]);
+
+  function fetchProductsList() {
+    axios.get(endpoint).then((response) => setProductsList(response.data));
+  }
+
+  useEffect(fetchProductsList, []);
+
   return (
-    <>
-      <h1>In questa pagina troverete la lista dei prodotti</h1>
-    </>
+    <section className="products-container">
+      {productsList.map((product) => {
+        const { id, title, price, description, category, image } = product;
+        return (
+          <div key={id} className="product-card">
+            <h6>{title}</h6>
+            <p>{price}</p>
+            <img src={image} alt={title} />
+            <p>{description}</p>
+            <p>{category}</p>
+          </div>
+        );
+      })}
+    </section>
   );
 }
 
